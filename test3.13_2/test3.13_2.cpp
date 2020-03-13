9b4f72d0d0624519c32bdbea756b8efa85cf240a
Cube <877211593@qq.com>
2020-03-13 15:48:54 +0800

    wu

--- test3.13_2/test3.13_2.cpp
+++ test3.13_2/test3.13_2.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
+using namespace cv;
 
 int main()
 {
+	cv::Mat srcMat = imread("E:\\Picture\\Camera Roll\\1.jpg");
+	cv::Mat grayMat;
+	cv::Mat result_one;
+	cv::Mat result_two;
+	
+	cvtColor(srcMat, grayMat, COLOR_BGR2GRAY);
+	threshold(grayMat,  result_one, 100, 255, THRESH_OTSU);
+	adaptiveThreshold(grayMat, result_two, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
+
+	imshow("gray", grayMat);
+	imshow("result_one", result_one);
+	imshow("result_two", result_two);
+
+	waitKey(0);
     return 0;
 }
 
